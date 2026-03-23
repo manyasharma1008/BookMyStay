8be91cf2c9892196040ddd7fb64c17abb2426ad6
manyasharma1008 <manya.s1008@gmail.com>
2026-03-23 15:39:39 +0530

    UC5 added by Manya Sharma

--- App.java
+++ App.java
@@ -1,90 +1,60 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class App {
 
-    public static abstract class Room {
-        protected int numberOfBeds;
-        protected int squareFeet;
-        protected double pricePerNight;
-
-        public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
-            this.numberOfBeds = numberOfBeds;
-            this.squareFeet = squareFeet;
-            this.pricePerNight = pricePerNight;
-        }
-
-        public void displayRoomDetails() {
-            System.out.println("Beds: " + numberOfBeds);
-            System.out.println("Size: " + squareFeet + " sqft");
-            System.out.println("Price per night: " + pricePerNight);
-        }
-    }
-
-    public static class SingleRoom extends Room {
-        public SingleRoom() {
-            super(1, 250, 1500.0);
-        }
-    }
-
-    public static class DoubleRoom extends Room {
-        public DoubleRoom() {
-            super(2, 400, 2500.0);
-        }
-    }
-
-    public static class Suite extends Room {
-        public Suite() {
-            super(3, 750, 5000.0);
-        }
-    }
-
-    public static class RoomInventory {
-        private Map<String, Integer> roomAvailability;
-
-        public RoomInventory() {
-            roomAvailability = new HashMap<>();
-        }
-
-        public void initializeInventory(String roomType, int available) {
-            roomAvailability.put(roomType, available);
-        }
-
-        public Map<String, Integer> getRoomAvailability() {
-            return roomAvailability;
-        }
-    }
-
     public static void main(String[] args) {
+        BookingRequestQueue queue = new BookingRequestQueue();
 
-        RoomInventory inventory = new RoomInventory();
-        inventory.initializeInventory("Single Room", 5);
-        inventory.initializeInventory("Double Room", 3);
-        inventory.initializeInventory("Suite Room", 2);
+        queue.addRequest(new Reservation("Abhi", "Single"));
+        queue.addRequest(new Reservation("Subha", "Double"));
+        queue.addRequest(new Reservation("Vanmathi", "Suite"));
 
-        System.out.println("Room Search\n");
+        System.out.println("Booking Request Queue");
 
-        Map<String, Integer> data = inventory.getRoomAvailability();
-
-        if (data.get("Single Room") > 0) {
-            System.out.println("Single Room:");
-            SingleRoom s = new SingleRoom();
-            s.displayRoomDetails();
-            System.out.println("Available: " + data.get("Single Room") + "\n");
+        while (!queue.isEmpty()) {
+            Reservation r = queue.processRequest();
+            System.out.println("Processing booking for Guest: " 
+                + r.getGuestName() + ", Room Type: " 
+                + r.getRoomType());
         }
+    }
+}
 
-        if (data.get("Double Room") > 0) {
-            System.out.println("Double Room:");
-            DoubleRoom d = new DoubleRoom();
-            d.displayRoomDetails();
-            System.out.println("Available: " + data.get("Double Room") + "\n");
-        }
+class Reservation {
+    private String guestName;
+    private String roomType;
 
-        if (data.get("Suite Room") > 0) {
-            System.out.println("Suite Room:");
-            Suite suite = new Suite();
-            suite.displayRoomDetails();
-            System.out.println("Available: " + data.get("Suite Room"));
-        }
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+class BookingRequestQueue {
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+    }
+
+    public Reservation processRequest() {
+        return requestQueue.poll();
+    }
+
+    public boolean isEmpty() {
+        return requestQueue.isEmpty();
     }
 }
