7aba1e8958dba6d017dee47fc2e18ca5fa3ab27a
manyasharma1008 <manya.s1008@gmail.com>
2026-03-23 15:06:10 +0530

    UC1 added by Manya Sharma

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialised successfully");
+    }
+}
